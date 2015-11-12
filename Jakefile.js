044511394a10627b68c7a5362744df1be3a1f2a9
Ross <Rossco@book.lan>
2015-11-12 13:34:25 +0000

    Linting jakefile.js only

--- Jakefile.js
+++ Jakefile.js
@@ -1,37 +1,71 @@
-
+/* globals task:false, desc:false, complete:false, fail:false,  jake:false */
 
 
 
 
 (function()
 {
+	"use strict";
 
 	var DEPLOY_DIR = ".";
+	var jsHint = require("simplebuild-jshint");
 
-	desc("Building Project: .")
+	desc("Building Project: .");
 	task("default",["lint"], function()
 	{
 		console.log("Build OK!");
-	})
+	});
 
-	desc("Launching HTTP: .")
+	desc("Launching HTTP: .");
 	task("http", function()
 	{
-		jake.exec("node_modules/http-server/bin/http-server/" + DEPLOY_DIR, {interactive:true}, complete );
-	})
+		//jake.exec("node_modules/http-server/bin/http-server/" + DEPLOY_DIR, {interactive:true}, complete );
+	});
 
-	desc("Linting JS Files: .")
+	desc("Linting JS Files: .");
 	task("lint", function()
 	{
-		//jake.exec("node_modules/jshint/bin/jshint ./**/*.js", complete, fail);
-	// /	jake.exec("./node_modules/jshint/bin/jshint ./Jakefile.js", {},complete );
-	})
+		console.log('Linting JS: .');
+
+		jsHint.checkFiles( { 	files:["Jakefile.js"],
+								options:lintingOptions(),
+								globals:lintingGlobals()
+							},	complete, fail);
+	});
 
 
-	desc("Checking Node Version: .")
-	task("checkver", function()
+
+	function lintingOptions()
 	{
+		return {
+		    "bitwise": "true",
+		    "eqeqeq": "true",
+		    "forin": "true",
+		    "freeze": "true",
+		    "futurehostile": "true",
+		    "latedef": "nofunc",
+		    "noarg": "true",
+		    "nocomma": "true",
+		    "nonbsp": "true",
+		    "nonew": "true",
+		    "strict": "true",
+		    "undef": "true",
+		    "node": "true",
+		    "browser": "true"
+  		};
+	}
 
-	})
-}())
+	function lintingGlobals()
+	{
+		return { 
+			describe:false,
+			it:false,
+			before:false,
+			beforeEach:false,
+			after:false,
+			afterEach:false
+		};	
+	}
+
+}());
 
