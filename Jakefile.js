b99511f7e15aeff091e0b3a3eb49b7a86fbbc233
Ross <Rossco@book.lan>
2015-11-12 13:08:14 +0000

    Added jshint options to package.json

--- Jakefile.js
+++ Jakefile.js
@@ -5,7 +5,7 @@
 
 (function()
 {
-	
+
 	var DEPLOY_DIR = ".";
 
 	desc("Launching HTTP: .")
@@ -13,6 +13,13 @@
 	{
 		jake.exec("node_modules/http-server/bin/http-server/" + DEPLOY_DIR, {interactive:true}, complete );
 	})
+
+	desc("Linting JS Files: .")
+	task("lint", function()
+	{
+		
+	})
+
 
 	desc("Checking Node Version: .")
 	task("checkver", function()
