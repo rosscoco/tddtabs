57b71338cd98cb1c7e517e547988173611114b57
Ross <Rossco@book.lan>
2015-11-12 13:03:35 +0000

    added jshint module

--- Jakefile.js
+++ Jakefile.js
@@ -1,12 +1,23 @@
 
-var DEPLOY_DIR = ".";
+
+
+
 
 (function()
 {
+	
+	var DEPLOY_DIR = ".";
+
 	desc("Launching HTTP: .")
 	task("http", function()
 	{
 		jake.exec("node_modules/http-server/bin/http-server/" + DEPLOY_DIR, {interactive:true}, complete );
+	})
+
+	desc("Checking Node Version: .")
+	task("checkver", function()
+	{
+
 	})
 }())
 
--- jake.sh
+++ jake.sh
@@ -1,4 +1,4 @@
 
 [ ! -f node_modules/.bin/jake ] && echo "Rebuilding npm modules" && npm rebuild
 
-node_modules/.bin/jake $*
+node_modules/.bin/jake $* 
