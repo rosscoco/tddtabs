e2a48d54411a1b49a1511e56a19213a3714b4c1c
Ross <Rossco@book.lan>
2015-11-12 15:11:38 +0000

    added commonjs module and separated addition and test files

--- src/addition.js
+++ src/addition.js
@@ -1,20 +1,11 @@
 (function()
 {
 	"use strict";
-	var assert = require('chai').assert;
 	
-	describe("Addition", function()
-	{
-		it("Adds two numbers", function()
-		{
-			assert.equal( add( 3,4 ), 7 );
-		});
-	});
-
 	function add(a,b)
 	{
 		return a + b;
 	}
 
-	
+	exports.add = add;
 }());
